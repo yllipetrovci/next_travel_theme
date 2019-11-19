6b695833f3866a682666d3f2f31536c622644d28
yllipetrovci <yllipetrovci@gmail.com>
2019-11-19 23:03:40 +0100

    init theme

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Add stylesheet
+ */
+function load_stylesheets()
+{
+    wp_register_style('stylesheet','style.css', '', 1, 'all');
+    wp_enqueue_style('stylesheet');
+}
+add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+/**
+ * Add javascript
+ */
+// function load_javascript()
+// {
+//     wp_register_script('custom', get_template_directory_uri() . '/app.js', 'jquery', 1, true);
+//     wp_enqueue_script('custom');
+// }
+// add_action('wp_enqueue_scripts', 'load_javascript');
+
+
+/**
+ * Add menus
+ */
+
+ add_theme_support('menus');
+
+/**
+ * Register Menus
+ */
+register_nav_menus(
+array(
+    'top-menu'=> 'Top Menu'
+)
+);
