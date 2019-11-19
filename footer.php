a305995ebb5707392a710811536ad1b5ac72cf0f
yllipetrovci <yllipetrovci@gmail.com>
2019-11-20 00:40:15 +0100

    implement footer

--- footer.php
+++ footer.php
@@ -1,6 +1,71 @@
-<footer>
-    <h2>Footer</h2>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <div class="footer--info-box">
+                    <h5 class="footer--info-box__h5">GERMANY</h5>
+                    <ul>
+                        <li>
+                            Eurokoha Reisen GmbH
+                        </li>
+                        <li>
+                            Baseler Str. 35-37
+                        </li>
+                        <li>
+                            60329 Frankfurt/M
+                        </li>
+                    </ul>
+                    <p class="footer-info-box__p"><b>Frankfurt</b> +49 (0)69 25 66 760</p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="footer--info-box">
+                    <h5 class="footer--info-box__h5">GERMANY</h5>
+                    <ul>
+                        <li>
+                            Eurokoha Reisen GmbH
+                        </li>
+                        <li>
+                            Baseler Str. 35-37
+                        </li>
+                        <li>
+                            60329 Frankfurt/M
+                        </li>
+                    </ul>
+                    <p class="footer-info-box__p"><b>Frankfurt</b> +49 (0)69 25 66 760</p>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="footer--info-box">
+                    <h5 class="footer--info-box__h5">GERMANY</h5>
+                    <ul>
+                        <li>
+                            Eurokoha Reisen GmbH
+                        </li>
+                        <li>
+                            Baseler Str. 35-37
+                        </li>
+                        <li>
+                            60329 Frankfurt/M
+                        </li>
+                    </ul>
+                    <p class="footer-info-box__p"><b>Frankfurt</b> +49 (0)69 25 66 760</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row ">
+        <div class="copyright">
+            <div class="social-media">
+
+            </div>
+            <!-- <h5 class="text-center">Copyright © 2019 Next Travel</h5> -->
+        </div>
+    </div>
+
+
 </footer>
 </body>
+<?php wp_footer(); ?>
 
 </html>
