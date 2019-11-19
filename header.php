caa242f13b20713c22d4309ae0fc7c291e305e45
yllipetrovci <yllipetrovci@gmail.com>
2019-11-19 23:07:23 +0100

    add meta tags

--- header.php
+++ header.php
@@ -1,7 +1,24 @@
 <html>
 
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <head>
+        <title></title>
+        <meta name="description" content="" />
+        <meta name="distribution" content="" />
+        <meta name="robots" content="All" />
+        <meta property="og:title" content="" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="" />
+        <meta property="og:url" content="" />
+        <meta property="og:site_name" content="" />
+        <meta property="og:type" content="article" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css' />
+    </head>
     <?php wp_head(); ?>
 </head>
 
